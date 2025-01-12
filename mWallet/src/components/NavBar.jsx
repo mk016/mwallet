6f4ced95f13edf013cebcb0a6f7acc52540d4f3a
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2025-01-12 08:02:42 +0000

    update navbar

--- mWallet/src/components/NavBar.jsx
+++ mWallet/src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import './NavBar.css'
 const NavBar = () => {
     return (
         <nav>
-            <span>OptiMask</span>
-            <a href="https://github.com/Fardeen26/OptiMask-Wallet.git" target="_blank"><button>Github</button></a>
+            <span>MWallet Generator</span>
+            <a href="https://github.com/mk016/mwallet" target="_blank"><button>Github</button></a>
         </nav>
     )
 }
